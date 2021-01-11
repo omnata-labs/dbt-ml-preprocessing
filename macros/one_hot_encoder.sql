a3b68f1e450f1b5d1e231256cd80f031ac5a4168
James Weakley <57749158+hh-jamesweakley@users.noreply.github.com>
2021-01-11 17:07:10 +1100

    Experimentation

--- macros/one_hot_encoder.sql
+++ macros/one_hot_encoder.sql
@@ -18,10 +18,25 @@ The `one_hot_encoder` macro only supports an 'handle_unknown' value of 'ignore' 
     {% endset %}
     {%- do exceptions.raise_compiler_error(error_message) -%}
 {%- endif -%}
+{{ adapter.dispatch('one_hot_encoder')(source_table,source_column,categories,handle_unknown,include_columns) }}
+{% endmacro %}
 
+{% macro default__one_hot_encoder(source_table,source_column,categories,handle_unknown,include_columns) %}
 select 
 {% for column in include_columns %}
 {{ source_table }}.{{ column }},
+{% endfor %}
+{% for category in category_values %}
+iff({{source_column}}='{{category}}',true,false) as {{source_column}}_{{category}}
+{% if not loop.last %}, {% endif %}
+{% endfor %}
+from {{ source_table }}
+{% endmacro %}
+
+{% macro bigquery__one_hot_encoder(source_table,source_column,categories,handle_unknown,include_columns) %}
+select 
+{% for column in include_columns %}
+{{ column }},
 {% endfor %}
 {% for category in category_values %}
 iff({{source_column}}='{{category}}',true,false) as {{source_column}}_{{category}}
