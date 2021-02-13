2524000490c934660bfa23038a124b57a9e71d80
comaraDOTcom <48843407+comaraDOTcom@users.noreply.github.com>
2021-02-13 14:35:37 +0000

    Adding drop_col, values args and removing whitespace in column names

--- macros/one_hot_encoder.sql
+++ macros/one_hot_encoder.sql
@@ -1,56 +1,59 @@
-{% macro one_hot_encoder(source_table,source_column,categories='auto',handle_unknown='ignore',include_columns='*') %}
-{%- if categories=='auto' -%}
-    {% set category_values_query %}
-    select distinct {{ source_column }} from {{ source_table }}
-    order by 1
-    {% endset %}
-    {% set results = run_query(category_values_query) %}
-    {% if execute %}
-        {# Return the first column #}
-        {% set category_values = results.columns[0].values() %}
-    {% else %}
-        {% set category_values = [] %}
-    {% endif %}
-{% elif categories is not iterable or categories is string or categories is mapping %}
-    {% set error_message %}
-The `categories` parameter must contain a list of category values.
-    {% endset %}
-    {%- do exceptions.raise_compiler_error(error_message) -%}
-{%- else -%}
-    {% set category_values = categories %}
-{%- endif -%}
-{%- if handle_unknown!='ignore' -%}
-    {% set error_message %}
-The `one_hot_encoder` macro only supports an 'handle_unknown' value of 'ignore' at this time.
-    {% endset %}
-    {%- do exceptions.raise_compiler_error(error_message) -%}
-{%- endif -%}
-{{ adapter.dispatch('one_hot_encoder',packages=['dbt_ml_preprocessing'])(source_table,source_column,category_values,handle_unknown,include_columns) }}
-{%- endmacro %}
+{% macro one_hot_encoder(source_table,source_column,categories='auto',handle_unknown='ignore',drop_col=none, value=none) %}
 
-{% macro snowflake__one_hot_encoder(source_table,source_column,category_values,handle_unknown,include_columns) %}
-select 
-{% for column in include_columns %}
-{{ source_table }}.{{ column }},
-{% endfor %}
-{% for category in category_values %}
-iff({{source_column}}='{{category}}',true,false) as {{source_column}}_{{category}}
-{% if not loop.last %}, {% endif %}
-{% endfor %}
-from {{ source_table }}
+    {%- if categories=='auto' -%}
+        {% set category_values_query %}
+        select distinct {{ source_column }} from {{ source_table }}
+        order by 1
+        {% endset %}
+        {% set results = run_query(category_values_query) %}
+        {% if execute %}
+            {# Return the first column #}
+            {% set category_values = results.columns[0].values() %}
+        {% else %}
+            {% set category_values = [] %}
+        {% endif %}
+    {% elif categories is not iterable or categories is string or categories is mapping %}
+        {% set error_message %}
+    The `categories` parameter must contain a list of category values.
+        {% endset %}
+        {%- do exceptions.raise_compiler_error(error_message) -%}
+    {%- else -%}
+        {% set category_values = categories %}
+    {%- endif -%}
+    {%- if handle_unknown!='ignore' -%}
+        {% set error_message %}
+    The `one_hot_encoder` macro only supports an 'handle_unknown' value of 'ignore' at this time.
+        {% endset %}
+        {%- do exceptions.raise_compiler_error(error_message) -%}
+    {%- endif -%}
+    {{ adapter.dispatch('one_hot_encoder',packages=['dbt_ml_preprocessing'])(source_table,source_column,category_values,handle_unknown,drop_col, value) }}
 {%- endmacro %}
 
 {% macro default__one_hot_encoder(source_table,source_column,category_values,handle_unknown,include_columns) %}
-select 
-{% for column in include_columns %}
-{{ column }},
-{% endfor %}
-{% for category in category_values %}
-case {{source_column}}
-    when '{{category}}' then true
-    else false
-    end as {{source_column}}_{{category}}
-{% if not loop.last %}, {% endif %}
-{% endfor %}
-from {{ source_table }}
+
+    {% set columns = adapter.get_columns_in_relation( source_table ) %}
+
+    with output_table as (
+        select
+            {% for column in columns %}
+                {%- if column.name | lower != col | lower %}
+                    {{ column.name }},
+                {%- endif -%}
+            {%- endfor -%}
+                {% if drop_col is none or not drop_col%}
+                {{col}},
+                {%- endif -%}
+            {%- for category in category_values -%}
+                {% set no_whitespace_column_name = category | replace( " ", "_") -%}
+                    %- if value is not none and value | lower  in columns | lower %}
+                    case when {{ col }} = '{{ category }}' then {{ value }} else 0 end as is_{{ col }}_{{ no_whitespace_column_name }}
+                {% else %}
+                    case when {{ col }} = '{{ category }}' then 1 else 0 end as is_{{ col }}_{{ no_whitespace_column_name }}
+                {%- endif -%}
+                {%- if not loop.last %},{% endif -%}
+            {% endfor %}
+    from {{ source_table }}
+)
+select * from output_table
+
 {%- endmacro %}
