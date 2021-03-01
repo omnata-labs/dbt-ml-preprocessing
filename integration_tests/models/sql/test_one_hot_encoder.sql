82c0a73b83cba0c6d0f63c1923cffc2d0fb03bb3
James Weakley <jameswillisweakley@gmail.com>
2021-03-01 14:47:51 +1100

    Change proposed approach to unknown value handing
    Instead of providing a is_{{ source_column }}_null column, implement the 'handle_unknown' parameter value 'error', to mirror scikit-learn behaviour.
    This means users must either handle missing values upstream, or accept that they create 'false' outputs in all columns.

--- integration_tests/models/sql/test_one_hot_encoder.sql
+++ integration_tests/models/sql/test_one_hot_encoder.sql
@@ -2,7 +2,7 @@
 
 with data as (
 
-    {{ dbt_ml_preprocessing.one_hot_encoder( ref('data_one_hot_encoder') ,'column_to_encode') }}
+    {{ dbt_ml_preprocessing.one_hot_encoder( ref('data_one_hot_encoder') ,'column_to_encode',handle_unknown='ignore') }}
 
 )
 
--- macros/one_hot_encoder.sql
+++ macros/one_hot_encoder.sql
@@ -1,4 +1,4 @@
-{% macro one_hot_encoder(source_table, source_column, categories='auto', handle_unknown='ignore',include_columns='*', exclude_columns=none) %}
+{% macro one_hot_encoder(source_table, source_column, categories='auto', handle_unknown='error',include_columns='*', exclude_columns=none) %}
 
     {%- if categories=='auto' -%}
         {% set category_values_query %}
@@ -23,30 +23,35 @@
     {%- else -%}
         {% set category_values = categories %}
     {%- endif -%}
-    {%- if handle_unknown!='ignore' -%}
+
+    {%- if handle_unknown!='ignore' and handle_unknown!='error' -%}
         {% set error_message %}
-    The `one_hot_encoder` macro only supports an 'handle_unknown' value of 'ignore' at this time.
+    The 'handle_unknown' parameter requires a value of either 'ignore' (when unknown value occurs, all output columns are false) or 'error' (when unknown value occurs, raise an error).
         {% endset %}
         {%- do exceptions.raise_compiler_error(error_message) -%}
     {%- endif -%}
-    {%- if include_columns!='*' or exclude_columns is not none -%}
-        {%- if include_columns!='*' and exclude_columns is not none -%}
-            {% set error_message %}
-        The `one_hot_encoder` macro only supports one of include_columns and exclude_columns being a non-default value or one of them being a list.
-            {% endset %}
-            {%- do exceptions.raise_compiler_error(error_message) -%}
-        {%- elif include_columns!='*' and (include_columns is not iterable  or include_columns is string or include_columns is mapping) -%}
-            {% set error_message %}
-        The `one_hot_encoder` macro only supports one of include_columns and exclude_columns being a non-default value or one of them being a list.
-            {% endset %}
-            {%- do exceptions.raise_compiler_error(error_message) -%}
-        {%- elif exclude_columns is not none and (exclude_columns is not iterable or exclude_columns is string or exclude_columns is mapping) -%}
-                    {% set error_message %}
-        The `one_hot_encoder` macro only supports one of include_columns and exclude_columns being a non-default value or one of them being a list.
-            {% endset %}
-            {%- do exceptions.raise_compiler_error(error_message) -%}
-        {%- endif -%}
+
+    {%- if include_columns!='*' and exclude_columns is not none -%}
+        {% set error_message %}
+    If the 'exclude_columns' parameter is set, providing 'include_columns' is invalid and must be left at its default value.
+        {% endset %}
+        {%- do exceptions.raise_compiler_error(error_message) -%}
     {%- endif -%}
+
+    {%- if exclude_columns is not none and (exclude_columns is not iterable or exclude_columns is string or exclude_columns is mapping) -%}
+        {% set error_message %}
+    The 'exclude_columns' parameter value contain a list of column names.
+        {% endset %}
+    {%- do exceptions.raise_compiler_error(error_message) -%}
+    {%- endif -%}
+
+    {%- if include_columns!='*' and (include_columns is not iterable or include_columns is string or include_columns is mapping) -%}
+        {% set error_message %}
+    The 'include_columns' parameter value must contain either the string '*' (for all columns in source), or a list of column names.
+        {% endset %}
+    {%- do exceptions.raise_compiler_error(error_message) -%}
+    {%- endif -%}
+
     {{ adapter.dispatch('one_hot_encoder',packages=['dbt_ml_preprocessing'])(source_table, source_column, category_values, handle_unknown, include_columns, exclude_columns) }}
 {%- endmacro %}
 
@@ -58,7 +63,7 @@
 
     with binary_output as (
     select
-        {%- if include_columns=='*' and exclude_columns is  none -%}
+        {%- if include_columns=='*' and exclude_columns is none -%}
             {% for column in columns %}
                 {{ column.name }},
             {%- endfor -%}
@@ -75,10 +80,18 @@
         {%- endif -%}
         {% for category in category_values %}
             {% set no_whitespace_column_name = category | replace( " ", "_") -%}
-                {%- if category is none %}
-                    case when {{ source_column }} is null then 1 else 0 end as is_{{ source_column }}_null
-                {% else %}
-                    case when {{ source_column }} = '{{ category }}' then 1 else 0 end as is_{{ source_column }}_{{ no_whitespace_column_name }}
+                {%- if handle_unknown=='ignore' %}
+                    case 
+                        when {{ source_column }} = '{{ category }}' then true 
+                        else false
+                    end as is_{{ source_column }}_{{ no_whitespace_column_name }}
+                {% endif %}
+                {%- if handle_unknown=='error' %}
+                    case 
+                        when {{ source_column }} = '{{ category }}' then true 
+                        when {{ source_column }} in ('{{ category_values | join("','") }}') then false
+                        else cast('Error: unknown value found and handle_unknown parameter was "error"' as boolean)
+                    end as is_{{ source_column }}_{{ no_whitespace_column_name }}
                 {% endif %}
             {%- if not loop.last %},{% endif -%}
         {% endfor %}
